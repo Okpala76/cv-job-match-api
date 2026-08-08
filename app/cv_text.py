6bcd39c0a00d9e17ee38f59e920929b7065f31c7
Gochi <86475712+Okpala76@users.noreply.github.com>
2026-08-08 09:40:14 +0100

    This is holds minor updates for the cv and marsks teh start of precious (#14)

--- app/cv_text.py
+++ app/cv_text.py
@@ -6,15 +6,13 @@ GITHUB: https://www.github.com/Okpala76      	LOCATION: LAGOS
 
 CAREER OBJECTIVE
 
-I am a passionate and results-driven Software Developer with an engineering foundation. I have under 3 years of experience in JavaScript, Python, and full-stack development
-
- Key Projects:
+ I am a passionate and results-driven Software Developer with an engineering foundation. I have just under 3 years of experience in JavaScript, Python, and full-stack development
+Key Projects:
 Propi: A platform connecting individuals with real estate professionals.
-NSO (No Seed Oil): An app to help users find healthier oil products.
-MaMeds: A medication reminder app.
-Gochi’s FPL Calendar: A fantasy football scheduling tool with fixture analysis.
+MaMeds: A medication reminder app(for web and mobile).
+Ajali Consulting Limited: A simple website built for simplicity and contact.
 PathFindAI: An AI-powered platform for project management and team collaboration.
-Studomia: A Learning Context Platform, that looks to revolutionize the learning space
+Studomia: A Learning Context Platform that looks to revolutionize the learning space
 
 EDUCATION
 MICHAEL OKPARA UNIVERSITY OF AGRICULTURE, UMUDIKE  [2016 - 2023]
@@ -27,8 +25,8 @@ WAECE (West African Examination Council Examination) For Certificate Click Here
 
 WORK EXPERIENCE
 EkoHacks Studios
-Engineer & Developer | 2024 AUGUST to 2026 MARCH 
-Led the Nigerian junior developers on the team carry out day to dat tasks
+Engineer & Developer | 2024 AUGUST– 2026 MARCH 
+Led the Nigerian junior developers on the team to carry out day-to-day tasks
 Propi: Full-stack development with Next.js (frontend) and FastAPI (backend).
 Led the integration of Supabase for database management and Railway for deployment automation.
 Implemented testing frameworks like Jest, Cypress, and Storybook to ensure code reliability.
@@ -46,19 +44,21 @@ Python Programmer
 Self-Employed | 2023 OCTOBER
 Developed Gochi’s FPL Calendar, a tool to create fantasy football schedules and analyze fixture difficulty.
 Utilized Python to automate data extraction from .txt files and generate dynamic schedules.
-Hosted the project on Twitter @allblacksports.
+Lords Heritage Assembly RCCG follow-up automation spreadsheet: An AI-integrated spreadsheet that automates follow-up for members 
+Winners Chapel Cell Fellowship follow-up automation spreadsheet: An AI-integrated spreadsheet that automates members follow-up 
 
 TECHNICAL SKILLS
 Languages: JavaScript (ES6+), Python, SQL
-Frontend: React, React Native, Expo, React Query, NextJS, Remix, Nuxt(VueJS),
+Frontend: React, React Native, Expo, React Query, NextJS, Remix, Nuxt(VueJS)
 Backend: Node.js, FastAPI, NestJS
 Databases & Cloud: Firebase (Auth, Firestore), Supabase, SQL databases
-Testing & Tooling: Jest, Cypress, Storybook, Git
-Other: REST APIs, ATS‑friendly documentation, Algorithmic problem‑solving (LeetCode), 
-Deployment: Docker, ORM(Drizzle, Prisma), Railway, Vercel. 
+Testing & Tooling: Jest, Cypress, Storybook, Git,  ORM(Drizzle, Prisma)
+Other: REST APIs, ATS‑friendly documentation, Algorithmic problem‑solving (LeetCode) 
+Deployment: Docker, Railway, Vercel. 
 
 LEADERSHIP & PROFESSIONAL AFFILIATIONS
-Tech Mentor & Knowledge Sharer: Created a course on ES6 updates for team development.
+Tech Mentor & Knowledge Sharer: Created a course on ES6 updates for team development
 Team Mentor, EkoHacks: Guided junior developers, reviewed PRs, and enforced coding best practices.
 IT Support, LASIEC (NYSC): Managed IT solutions, ensuring seamless network and hardware maintenance.
+
 """
